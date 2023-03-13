53c76f49016215b6a739af190f59f0e72f750574
Victor Gabriel <105755567+victorZoro@users.noreply.github.com>
2023-03-13 11:02:16 -0300

    Criando a pasta de trabalho para o jogo Batalha Naval
    Criada a pasta que será usada para desenvolver o jogo Batalha Naval, como descrito pelo roteiro do Trabalho Prático 1.

---
+++ tp_01/desktop/src/com/grupolaz/batalhanaval/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.grupolaz.batalhanaval;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.grupolaz.batalhanaval.BatalhaNaval;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Batalha Naval");
+		new Lwjgl3Application(new BatalhaNaval(), config);
+	}
+}
