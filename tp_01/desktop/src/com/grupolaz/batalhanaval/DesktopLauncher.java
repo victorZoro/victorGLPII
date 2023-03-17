e9431e0b100a384d7e1d58963b604a901c8e5ade
Victor Gabriel <105755567+victorZoro@users.noreply.github.com>
2023-03-17 20:12:54 -0300

    Alterações iniciais no código.
    Em equipe, editamos o código, baseando-nos no código de batalha naval disponível em: <https://github.com/meh2481/JavaBship>. Criamos a classe Navio e começamos a editar o código principal do jogo. Alterações posteriores no código principal são esperadas.

--- tp_01/desktop/src/com/grupolaz/batalhanaval/DesktopLauncher.java
+++ tp_01/desktop/src/com/grupolaz/batalhanaval/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Batalha Naval");
+		config.setWindowedMode(640, 640);
+		config.useVsync(true);
 		new Lwjgl3Application(new BatalhaNaval(), config);
 	}
 }
