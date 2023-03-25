29ae4d7cf5acbece57dccf8a3b6e9d6ce66353ac
Victor Gabriel <105755567+victorZoro@users.noreply.github.com>
2023-03-25 20:38:02 -0300

    Nova tentativa de criação de jogo Batalha Naval
    Após uma tentativa fracassada de criar o jogo Batalha Naval utilizando a libGDX, resolvemos recomeçar e mudar a técnica. Ao invés de programar blocos de códigos e depois testarmos, entramos em um acordo de fazer testes pequenos e graduais do código em funcionamento. Pode parecer óbvio, mas, a primeira vista, a biblioteca pode ser muito complicada, e pensar em formas de testar gradualmente é bem desafiante para novatos nessa área.
    
    A classe Table (tabuleiro) foi desenvolvida a ponto de estar quase finalizada e funcional. Agora é possível renderizar a imagem de fundo e identificar cliques em uma certa posição. No entanto, além dos cliques ainda não serem muito relevantes, visto que não há navios, a parte específica do código responsável por mostrar em que casa do tabuleiro o mouse se localiza está gerando problemas.
    
    Também precisamos conversar com nosso professor sobre a utilização da função que identifica os cliques, que foi adaptada de um código disponibilizado a nós pelo mesmo.
    
    Concluindo, houve muito avanço no entendimentod a biblioteca, na pesquisa e desenvolvimento do jogo no dia de hoje.

--- tentativa2_tp01/desktop/src/com/grupolaz/batalhanaval/DesktopLauncher.java
+++ tentativa2_tp01/desktop/src/com/grupolaz/batalhanaval/DesktopLauncher.java
@@ -4,12 +4,29 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.grupolaz.batalhanaval.BatalhaNaval;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/**
+ * Código do Jogo Batalha Naval
+ * Trabalho Prático de No. 1 de LP II (Linguagens e Técnicas de Programação II)
+ * Centro Federal de Ensino Tecnológico de Minas Gerais Campus Contagem
+ * Turma: INFORMÁTICA 3 (2023)
+ * @author: Anna Luisa Andrade da Silva
+ * @author: Lucas Vinicios Santos da Cruz
+ * @author: Victor Gabriel Ribeiro Mariano
+ * @version: 0.0.1
+ */
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Batalha Naval");
+
+		config.setTitle("Batalha Naval"); // Configura o título
+
+		config.setForegroundFPS(60); // Configura a quantidade de quadros por segundo
+
+		config.setWindowedMode(640, 640); // Configura o tamanho da tela em modo janela
+
+		config.setResizable(false); // Configura para que a tela não seja redimensionável
+
 		new Lwjgl3Application(new BatalhaNaval(), config);
 	}
 }
